29a01c0dcff661f4af4f7619f4826857ee2b4446
Jakesyl <jakesyl@gmail.com>
2014-10-06 18:40:40 -0400

    daily file

---
+++ src/main.py
@@ -0,0 +1,18 @@
+from  urllib.request import urlopen
+import datetime 
+from time import gmtime, strftime
+from random import randrange as ran
+import subprocess as sp
+
+class main:
+    def main_func(self):
+        response = urlopen('http://www.iheartquotes.com/api/v1/random')
+        html = response.read()
+        filename = str(strftime("%Y-%m-%d", gmtime()))
+        filename += str(ran(90))
+        with open(filename, mode = 'a', encoding = 'utf-8') as a_file:
+            a_file.write(str(html))
+        gitpush = sp.check_output('git add -A && git commit -m "daily file" && git push', stderr=sp.STDOUT, shell=True)
+        print(gitpush)
+mc = main()
+print(main.main_func(mc))
